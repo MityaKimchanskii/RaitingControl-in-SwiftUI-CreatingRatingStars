9e905bcefe037ae7c216286e7cd6da01eef29283
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-21 23:51:02 -0700

    Initial Commit

---
+++ RaitingControl-in-SwiftUI/RaitingControl_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaitingControl_in_SwiftUIApp.swift
+//  RaitingControl-in-SwiftUI
+//
+//  Created by Mitya Kim on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct RaitingControl_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
